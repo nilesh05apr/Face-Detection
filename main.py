3983fcf1b1ad20903d9a3bb1b164eca5e6f9ffc8
nilesh05apr <nk8588338@gmail.com>
2022-01-17 17:48:26 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+
+
+def detect(img):
+    gry_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    face = face_cascade.detectMultiScale(gry_img, 1.05, 5)
+
+    for x, y, w, h in face:
+        img = cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    resized_img = cv.resize(img, (int(img.shape[0] / 2), int(img.shape[1] / 2)))
+    return resized_img
+
+
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+img_path = 'images/Image_1.jpg'
+img = cv.imread(img_path, 1)
+faces = detect(img)
+# cv.imshow('image',faces)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+output_path = 'output/'
+cv.imwrite(output_path + 'sample output.jpg', faces)
